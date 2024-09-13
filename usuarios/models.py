465d4c313bf4c457329b4decfc13cdba113eeb13
lucasimroth <lucas-imroth@outlook.com.br>
2024-09-13 18:52:52 -0300

    ajustes no README e adição de um botao em login para retorno ao cadastro

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     STATUS_CHOICES = [
         ('Pendente', 'Pendente'),
